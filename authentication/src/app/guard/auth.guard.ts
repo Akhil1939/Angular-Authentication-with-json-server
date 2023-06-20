bdb4036a1300f116b23368a76d347b3163a17a85
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-06-20 17:42:47 +0530

    1.0

--- authentication/src/app/guard/auth.guard.ts
+++ authentication/src/app/guard/auth.guard.ts
@@ -1,21 +1,23 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const service = inject(AuthService);
   const router = inject(Router);
+  const toast = inject(ToastrService);
+
   if (!service.IsLoggedIn()) {
     router.navigate(['/login']);
     return false;
   }
 
-  if(service.GetUserRole() != 'admin'){
+  if (service.GetUserRole() != 'admin') {
+    toast.error('You are not authorized to access this page');
     router.navigate(['']);
-    
-  return false;
 
+    return false;
   }
-  return true
-
+  return true;
 };
